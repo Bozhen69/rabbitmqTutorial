59d86f5456bb87ae946548438d3e94e3835ae345
Боженко Павел Павлович <bozhenko@reksoft.ru>
2020-12-21 22:54:23 +0300

    6 tutorial six Tutorial Rabbitmq

--- src/main/java/com/bohzen/rabbitClient/Main.java
+++ src/main/java/com/bohzen/rabbitClient/Main.java
@@ -1,6 +1,6 @@
 package com.bohzen.rabbitClient;
 
-import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -13,21 +13,53 @@ import java.util.concurrent.TimeoutException;
  * @author Pavel Bozhenko <bozhenko@rekfost.ru>
  */
 public class Main {
-    private static final String TOPIC_NAME = "topic_logs";
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
 
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-        channel.exchangeDeclare(TOPIC_NAME, BuiltinExchangeType.TOPIC);
-        String queueName = channel.queueDeclare().getQueue();
-        channel.queueBind(queueName, TOPIC_NAME, "*1.*1.*1");
-        DeliverCallback callback = (consumerTag, message) -> {
-            String text = new String(message.getBody(), StandardCharsets.UTF_8);
-            System.out.println(" [x] Received '" + text + "'");
+        channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+        channel.queuePurge(RPC_QUEUE_NAME);
+        channel.basicQos(1);
+        System.out.println(" [x] Awaiting RPC requests");
+        Object monitor = new Object();
+        DeliverCallback callback = (consumerTag, delivery) -> {
+            AMQP.BasicProperties replyProps = new AMQP.BasicProperties
+                    .Builder()
+                    .correlationId(delivery.getProperties().getCorrelationId())
+                    .build();
+            String response = "";
+            try {
+                String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                int req = Integer.parseInt(message);
+                response += fib(req);
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+            } finally {
+                channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, response.getBytes());
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                synchronized (monitor) {
+                    monitor.notify();
+                }
+            }
         };
-        channel.basicConsume(queueName, true, callback, consumerTag -> {
-        });
+
+        channel.basicConsume(RPC_QUEUE_NAME, false, callback, (consumerTag -> {
+        }));
+        while (true) {
+            synchronized (monitor) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static int fib(int n) {
+        return n == 0 ? 0 : (n == 1 ? 1 : fib(n - 1) + fib(n - 2));
     }
 }
--- src/main/java/com/bohzen/rabbitSender/Main.java
+++ src/main/java/com/bohzen/rabbitSender/Main.java
@@ -1,30 +1,47 @@
 package com.bohzen.rabbitSender;
 
-import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import java.io.IOException;
-import java.util.Random;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeoutException;
 
 /**
  * @author Pavel Bozhenko <bozhenko@rekfost.ru>
  */
 public class Main {
-    private static final String TOPIC_NAME = "topic_logs";
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
 
-    public static void main(String[] args) throws IOException, TimeoutException {
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(TOPIC_NAME, BuiltinExchangeType.TOPIC);
-            String message = args.length < 1 ? "log: Hello World!" :
-                    String.join(" ", args);
-            for(int i=0;i<5;i++) {
-                channel.basicPublish(TOPIC_NAME, "*1.*1.*1", null, (message+" - "+i).getBytes());
-                System.out.println("[X] Sent '" + message + "'");
+            for(int i=0;i<32;i++) {
+                System.out.println(" [x] Requesting fib(" + i + ")");
+                final String replyQueueName = channel.queueDeclare().getQueue();
+                final String corrId = UUID.randomUUID().toString();
+                AMQP.BasicProperties replyProps = new AMQP.BasicProperties
+                        .Builder()
+                        .replyTo(replyQueueName)
+                        .correlationId(corrId)
+                        .build();
+                channel.basicPublish("", RPC_QUEUE_NAME, replyProps, String.valueOf(i).getBytes(StandardCharsets.UTF_8));
+                final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
+                String ctag = channel.basicConsume(replyQueueName, true, (consumerTag, delivery) -> {
+                    if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                        response.offer(new String(delivery.getBody(), StandardCharsets.UTF_8));
+                    }
+                }, consumerTag -> {
+                });
+                String result = response.take();
+                channel.basicCancel(ctag);
+                System.out.println(" [.] Got '" + result + "'");
             }
         }
     }
