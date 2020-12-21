07fabfa24ab3c587109ac95c2fad309eaa7bd603
Боженко Павел Павлович <bozhenko@reksoft.ru>
2020-12-21 19:07:43 +0300

    4 tutorial Forth Tutorial Rabbitmq

--- src/main/java/com/bohzen/rabbitClient/Main.java
+++ src/main/java/com/bohzen/rabbitClient/Main.java
@@ -6,22 +6,26 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Random;
 import java.util.concurrent.TimeoutException;
 
 /**
  * @author Pavel Bozhenko <bozhenko@rekfost.ru>
  */
 public class Main {
-    private static final String EXCHANGE_NAME = "logs";
+    private static final String EXCHANGE_NAME = "logs2";
+    private static final String[] rout = new String[]{"crit","normal"};
 
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
+        String routKey = rout[new Random().nextInt(2)];
+        System.out.println("rout key is - " + routKey);
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-        channel.exchangeDeclare(EXCHANGE_NAME,"fanout");
+        channel.exchangeDeclare(EXCHANGE_NAME,"direct");
         String queueName = channel.queueDeclare().getQueue();
-        channel.queueBind(queueName,EXCHANGE_NAME,"");
+        channel.queueBind(queueName,EXCHANGE_NAME,routKey);
         DeliverCallback callback = (consumerTag, message) -> {
             String text = new String(message.getBody(), StandardCharsets.UTF_8);
             System.out.println(" [x] Received '" + text + "'");
--- src/main/java/com/bohzen/rabbitSender/Main.java
+++ src/main/java/com/bohzen/rabbitSender/Main.java
@@ -5,24 +5,26 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import java.io.IOException;
+import java.util.Random;
 import java.util.concurrent.TimeoutException;
 
 /**
  * @author Pavel Bozhenko <bozhenko@rekfost.ru>
  */
 public class Main {
-    private static final String EXCHANGE_NAME = "logs";
+    private static final String EXCHANGE_NAME = "logs2";
+    private static final String[] rout = new String[]{"crit","normal"};
 
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
-            String message = args.length < 1 ? "info: Hello World!" :
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+            String message = args.length < 1 ? "log: Hello World!" :
                     String.join(" ", args);
             for(int i=0;i<5;i++) {
-                channel.basicPublish(EXCHANGE_NAME, "", null, (message+" - "+i).getBytes());
+                channel.basicPublish(EXCHANGE_NAME, rout[new Random().nextInt(2)], null, (message+" - "+i).getBytes());
                 System.out.println("[X] Sent '" + message + "'");
             }
         }
